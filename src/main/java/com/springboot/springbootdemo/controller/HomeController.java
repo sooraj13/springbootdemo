f06e7f092255ed894b3157a174ee29c0c66b7a64
sooraj13 <55126839+sooraj13@users.noreply.github.com>
2021-09-29 01:06:36 +0530

    Add files via upload

---
+++ src/main/java/com/springboot/springbootdemo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springboot.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hello World From SPringBoot App";
+	}
+
+}
